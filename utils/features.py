22f7589f75ae2910ef12c192e46e5ec830fca22d
hsm207 <hsm207@users.noreply.github.com>
2017-10-02 15:43:29 -0400

    Created a function to extract the positional embeddings from the tokens column.

--- utils/features.py
+++ utils/features.py
@@ -48,3 +48,23 @@ def get_words_between_entities(row):
 
     words_between_entities = word_tokenize(words_between_entities)
     return pd.Series({'tokens': words_between_entities})
+
+
+def get_positional_embeddings(row):
+    """
+    Compute the positional embeddings given a list of words that are between a pair of entities. This function is
+    meant to be called via a panda's dataframe.apply() function where the dataframe is a single column named
+     'tokens' containing the list of words.
+
+    :param row: A row from the pandas dataframe that is passed to the call to apply()
+    :return: A two column dataframe where column 1 is the positional embedding relative to entity 1 and column 2
+             is the positional embedding relative to entity 2
+    """
+    word_list = row['tokens']
+    n_words_between_entities = len(word_list)
+
+    relative_position_e1 = np.arange(1, n_words_between_entities + 1)
+    relative_position_e2 = np.flipud(-np.arange(1, n_words_between_entities + 1))
+
+    return pd.Series({'relative_position_e1': relative_position_e1,
+                      'relative_position_e2': relative_position_e2})
